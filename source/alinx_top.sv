a34fae2c96a40fb1d463aa965a0ee13a2ef12be3
Min Cai <min.cai.china@gmail.com>
2016-02-13 21:15:15 +0800

    Remove the simulator code.

--- source/alinx_top.sv
+++ source/alinx_top.sv
@@ -1,14 +1,27 @@
+`include "config.sv"
+
 module alinx_top(input logic clk, reset_n,
 			output logic [3:0] led);
 
-  assign led[3:2] = 2'b0;
-
-  // instantiate the simulator
+  // Network Read from Nodes.  Valid/Enable protocol.
+  // ------------------------------------------------------------------------------------------------------------------
+  packet_t [0:`NODES-1] i_data; // Input data from the nodes to the network
+  logic [0:`NODES-1] i_data_val; // Validates the input data from the nodes.
+  logic [0:`NODES-1] o_en; // Enables the node to send data to the network.
   
-  parameter MAX_CYCLE_WIDTH = 32;
+  // Network Write to Nodes.  Valid/Enable protocol.
+  // ------------------------------------------------------------------------------------------------------------------
+  packet_t [0:`NODES-1] o_data; // Output data from the network to the nodes
+  logic [0:`NODES-1] o_data_val; // Validates the output data to the nodes
   
-  logic [MAX_CYCLE_WIDTH-1:0] current_cycle;
-  
-  simulator #(MAX_CYCLE_WIDTH) simulator(clk, reset_n, led[1:0], current_cycle);
+  network network(
+						.clk(clk), 
+						.reset_n(reset_n), 
+						.i_data(i_data), 
+						.i_data_val(i_data_val),
+						.o_en(o_en),
+						.o_data(o_data),
+						.o_data_val(o_data_val)
+                );
 			
 endmodule
